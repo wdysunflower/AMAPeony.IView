ad47c02e9d397609c60e50cab62e91fdf5bb1abf
wdysunflower <wdy3449124>
2014-08-12 19:20:29 +0800

    【调整】注册、登录界面

--- app/src/main/java/com/peony/iview/fragment/RegisterFragment.java
+++ app/src/main/java/com/peony/iview/fragment/RegisterFragment.java
@@ -47,7 +47,7 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_sign, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         mLoginFormView = view.findViewById(R.id.login_form);
         mProgressView = view.findViewById(R.id.login_progress);
 
