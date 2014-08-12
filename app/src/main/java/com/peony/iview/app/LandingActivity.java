cbb4d4f6342b045e8f723d44a36eaaf1f5223291
wdysunflower <wdy3449124>
2014-08-12 16:36:52 +0800

    【新增】修改App图标、名称

--- app/src/main/java/com/peony/iview/app/LandingActivity.java
+++ app/src/main/java/com/peony/iview/app/LandingActivity.java
@@ -2,6 +2,7 @@ package com.peony.iview.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
@@ -31,6 +32,7 @@ public class LandingActivity extends Activity implements OnPageListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_landing);
 
         InitialPageView();
