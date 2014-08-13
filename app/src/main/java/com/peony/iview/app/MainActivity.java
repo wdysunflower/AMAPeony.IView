0b447bc6a00af1300d05357cdba9468619af5c98
wdysunflower <wdy3449124>
2014-08-13 17:03:20 +0800

    【新增】主界面Tab布局

--- app/src/main/java/com/peony/iview/app/MainActivity.java
+++ app/src/main/java/com/peony/iview/app/MainActivity.java
@@ -1,41 +1,74 @@
 package com.peony.iview.app;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.app.LocalActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 import com.peony.iview.global.ShowToastUtil;
+import com.peony.iview.module.ActionActivity;
+import com.peony.iview.module.GroupActivity;
+import com.peony.iview.module.MineActivity;
+import com.peony.iview.module.MoreActivity;
+import com.peony.iview.module.RouteActivity;
 
 public class MainActivity extends Activity {
+    private TabHost tabHost;
+    private TabWidget widget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        tabHost = (TabHost) this.findViewById(android.R.id.tabhost);
 
-        ActionBar actionBar = this.getActionBar();
-        actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP, ActionBar.DISPLAY_HOME_AS_UP);
+        LocalActivityManager mLocalActivityManager = new LocalActivityManager(this, false);
+        mLocalActivityManager.dispatchCreate(savedInstanceState);
+        tabHost.setup(mLocalActivityManager);
+        InitialTab();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void InitialTab() {
+        Intent it_one = new Intent(this, ActionActivity.class);
+        String action = getResources().getString(R.string.module_activity_action);
+        addTab(it_one, action);
+
+        Intent it_two = new Intent(this, RouteActivity.class);
+        String route = getResources().getString(R.string.module_activity_route);
+        addTab(it_two, route);
+
+        Intent it_three = new Intent(this, GroupActivity.class);
+        String group = getResources().getString(R.string.module_activity_group);
+        addTab(it_three, group);
+
+        Intent it_four = new Intent(this, MineActivity.class);
+        String mine = getResources().getString(R.string.module_activity_mine);
+        addTab(it_four, mine);
+
+        Intent it_five = new Intent(this, MoreActivity.class);
+        String more = getResources().getString(R.string.module_activity_more);
+        addTab(it_five, more);
+
+        tabHost.setCurrentTab(0);
+        widget = tabHost.getTabWidget();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // 当ActionBar图标被点击时调用
-                System.out.println("点击了Home按钮！");
-                break;
-        }
-        return super.onOptionsItemSelected(item);
+    private void addTab(Intent intent, String name) {
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec(name);
+        tabSpec.setContent(intent);
+        View view = getLayoutInflater().inflate(
+                R.layout.maintab_textview, null);
+        TextView textView =(TextView) view.findViewById(R.id.tab_Label);
+        textView.setText(name);
+        tabSpec.setIndicator(view);
+        tabHost.addTab(tabSpec);
     }
 
     private long exitTime = 0;
@@ -56,4 +89,15 @@ public class MainActivity extends Activity {
         return super.onKeyDown(keyCode, event);
     }
 
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && event.getRepeatCount() == 0) {
+                onKeyDown(KeyEvent.KEYCODE_BACK, event);
+                return true;
+            }
+        }
+        return super.dispatchKeyEvent(event);
+    }
 }
---
+++ app/src/main/java/com/peony/iview/module/MineActivity.java
@@ -0,0 +1,41 @@
+package com.peony.iview.module;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.peony.iview.app.R;
+
+public class MineActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_mine);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mine, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
