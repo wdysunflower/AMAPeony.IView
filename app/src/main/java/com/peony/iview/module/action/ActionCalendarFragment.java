9dd1e4ac71022281632c8a0a00208bd3dcde92f7
wdysunflower <wdy3449124>
2014-08-15 14:05:12 +0800

    【新增】系统日历展示

--- app/src/main/java/com/peony/iview/module/action/ActionCalendarFragment.java
+++ app/src/main/java/com/peony/iview/module/action/ActionCalendarFragment.java
@@ -1,43 +1,38 @@
 package com.peony.iview.module.action;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.CalendarView;
 
-import com.peony.iview.Adapter.ActionListAdapter;
 import com.peony.iview.app.R;
 
-public class ActionListFragment extends Fragment {
+public class ActionCalendarFragment extends Fragment {
     private Activity parentActivity;
 
-    private ListView mListView;
+    private CalendarView mCalendar;
 
-    public ActionListFragment() {
+    public ActionCalendarFragment() {
         // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_action_list, container, false);
-        mListView = (ListView) view.findViewById(R.id.action_list_view);
-        initialListView();
+        View view = inflater.inflate(R.layout.fragment_action_calendar, container, false);
+       // mCalendar = (CalendarView) view.findViewById(R.id.action_calendar_view);
         return view;
     }
 
-    private void initialListView() {
-        mListView.setAdapter(new ActionListAdapter(parentActivity));
-    }
 
     @Override
     public void onAttach(Activity activity) {
