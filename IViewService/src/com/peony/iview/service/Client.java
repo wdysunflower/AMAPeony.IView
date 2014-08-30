04e21d8682a6e9f01d3c6a836931e7e754755e5e
yangguang <5201314lpcwl@gmail.com>
2014-08-30 10:26:50 +0800

    IViewService: Client添加超时设置

--- IViewService/src/com/peony/iview/service/Client.java
+++ IViewService/src/com/peony/iview/service/Client.java
@@ -10,6 +10,7 @@ import org.apache.thrift.protocol.TBinaryProtocol;
  * IView客户端. 使用该类与服务器进行交互并获得各种服务.
  * 在获取各种服务前请首先调用connect函数与服务器建立连接.
  * 本类中所有与服务器之间通讯的操作均为同步操作. 即服务器返回结果后,相应函数才返回.
+ * 因此为了避免服务器由于长时间不返回结果造成的客户端阻塞. 请调用setTimeout设置超时时间.
  */
 public class Client {
 
@@ -17,6 +18,7 @@ public class Client {
      * 建立同服务器之间的连接.
      * 注意在该操作中设置的timeout值,会对后续与服务器交互操作的超时时间产生影响.
      * 例如调用本函数时设置timeout为3000,后续的login或者logout等与服务器交互操作的超时时间为3000.
+     * 如想在连接完成后修改操作超时时间，请调用setTimeout函数.
      * 如果在调用本函数后没有调用close函数再次调用本函数,本函数的语义为在内部调用close关闭连接,
      * 然后建立新的连接.
      * @param host 服务器主机地址. 可以是ip、主机名称或者域名.
@@ -41,6 +43,14 @@ public class Client {
         transport = null;
         client = null;
         userId = -1;
+    }
+
+    /**
+     * 设置操作超时时间.
+     * @param timeout 操作超时时间. 单位为ms.
+     */
+    public void setTimeout(int timeout) {
+        transport.setTimeout(timeout);
     }
 
     /**
@@ -78,7 +88,7 @@ public class Client {
         userId = -1;
     }
 
-    TTransport transport = null;
+    TSocket transport = null;
     IViewService.Client client = null;
     long userId = -1;
 }
